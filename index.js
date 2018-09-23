110fccc5ab9c006fe0e67578bf6d29c2021dbb84
Irvin Torcuato <irvin@wiwi.ninja>
2018-09-23 22:41:54 +0800

    Initial commit added watch face

---
+++ index.js
@@ -0,0 +1,9 @@
+function adjustTime() {
+  const secondHand = document.querySelector('svg line:nth-of-type(1)');
+  const hourHand = document.querySelector('svg line:nth-of-type(2)');
+
+
+}
+
+setInterval(adjustTime, 1000);
+
